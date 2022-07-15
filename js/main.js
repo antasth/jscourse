e247fe6b4fd1a38b33e95c3e55436342a7d8bd45
antasth <antasth@mail.ru>
2022-07-15 12:50:22 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,66 @@
+"use strict";
+
+// const obj = {
+//   name: 'John',
+//   age: 25,
+//   isMarried: false 
+// };
+
+// console.log(obj.name);
+// console.log(obj.name);
+
+// let arr = ['plum.png', 'orange.jpg', 6, {}, [], 'apple.bmp'];
+// console.log(arr[2]);
+
+// alert('hello');
+
+// const result = confirm('Чик чирик?');
+// console.log(result);
+
+// const answer = +prompt('Че бля', '18');
+// console.log(typeof(answer + '5') );
+
+// const answers = [];
+
+//  answers[0] = prompt('Имя ?', '');
+//  answers[1] = prompt('фамилие ?', '');
+//  answers[2] = prompt('откуда будешь ?', '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5/uuu`);
+
+// const user = 'qwer';
+
+// alert(`Превед , ${user}`);
+
+// 
+// let incr = 10,
+//     decr = 10;
+
+//    //  ++incr;
+//    //  --decr;
+
+
+//     console.log(++incr);
+//     console.log(--decr);
+
+// let i = 0;
+
+//     for(i=0; i++; i<10){
+//       alert(i);
+//     }
+
+//     console.log(5%2);
+
+//     console.log(2*4 === '8');
+
+   //  const isChecked = false,
+   //        isClose = false;
+
+   // console.log(isChecked || !isClose);
+
+   // console.log(2 + 2 * 2 !== 8);
+
