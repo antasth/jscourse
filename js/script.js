cffafe3963725cb2897883de4975aaf840a6fca2
antasth <antasth@mail.ru>
2022-07-16 14:40:47 +0300

    script.js add questions in cycle

--- js/script.js
+++ js/script.js
@@ -1,53 +1,57 @@
 'use strict';
-/* Задание на урок:
 
-1) Создать переменную numberOfFilms 
-и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'*/
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const numberOfFilms  = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-/*2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false*/
+let lastSeenFilm,
+    lastSeenFilmRate;
 
-   const personalMovieDB = {
-      count: numberOfFilms,
-      movies: {},
-      actors: {},
-      genres: [],
-      privat: false
-    };
+for (let i = 1; i < 3; i++) {
+    lastSeenFilm = prompt('Один из последних просмотренных фильмов?', '');
+    lastSeenFilmRate = prompt('На сколько оцените его?', '');
 
-/*3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }*/
-
-const lastSeenFilm      = prompt('Один из последних просмотренных фильмов?',''),
-      lastSeenFilmRate  = prompt('На сколько оцените его?',''),
-      lastSeenFilm2     = prompt('Один из последних просмотренных фильмов?'),
-      lastSeenFilmRate2 = prompt('На сколько оцените его?');
-
-personalMovieDB.movies[lastSeenFilm] = lastSeenFilmRate;
-personalMovieDB.movies[lastSeenFilm2] = lastSeenFilmRate2;
-
-/*Проверить, чтобы все работало без ошибок в консоли */
-
-console.log(personalMovieDB);
-
-
-if(4 == 9) {
-   console.log('OK');
-} else {
-   console.log('ERROR');
+    if ((lastSeenFilm != '' && lastSeenFilm != null) &&
+        (lastSeenFilmRate != '' && lastSeenFilmRate != null) &&
+        (lastSeenFilm.length < 10)) {
+        personalMovieDB.movies[lastSeenFilm] = lastSeenFilmRate;
+    } else {
+        alert("неверное значение");
+        i--;
+    }
 }
 
+if (personalMovieDB.count < 10) {
+    alert(`Просмотрено ${personalMovieDB.count} фильмов это довольно мало `);
+} else if (personalMovieDB.count > 30) {
+    alert(`Просмотрено ${personalMovieDB.count} фильмов это довольно много `);
+} else {
+    alert("Вы классический зритель");
+}
 
+console.log(personalMovieDB);
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. 
+Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, 
+и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - 
+"Вы классический зритель", 
+а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами
+
+Код возьмите из предыдущего домашнего задания*/
