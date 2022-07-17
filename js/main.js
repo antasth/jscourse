648612012aa61f6cdaa3ca32963013bd02d37c25
antasth <antasth@mail.ru>
2022-07-17 22:12:51 +0300

    replace cycles on functions

--- js/main.js
+++ js/main.js
@@ -113,3 +113,75 @@
 //    }
 //    console.log(i);
 // }
+
+// ! FUNCTIONS
+// let num = 20;
+
+// function showFirstMessage(text) {
+//    console.log(text);
+//    console.log(num);
+// }
+
+// showFirstMessage('Hello');
+// console.log(num);
+
+// function calc(a, b) {
+//    return(a + b);
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+// function ret() {
+//    let num = 50;
+//    return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function() {
+//    console.log('hello');
+// };
+
+// logger();
+
+// const calc = (a, b) => {
+//    return a + b;
+// };
+
+//  console.log(calc(6, 3));
+
+
+// // ! МЕТОДЫ И СВОЙСТВА СТРОК И ЧИСЕЛ
+
+// const str = 'test';
+// const arr = [1, 2 ,4];
+
+// console.log(str.length);
+// console.log(arr.length);
+// console.log(str[2]);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// console.log(str);
+
+// const fruit = 'Some fruit';
+
+// console.log(fruit.indexOf('fruit'));
+// console.log(fruit.indexOf('q'));
+
+// const logg = 'hello world';
+
+// console.log(logg.slice(6, 11));
+// console.log(logg.substring(6, 11));
+// console.log(logg.substr(6, 5));
+
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+// const test = '12.2px';
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
